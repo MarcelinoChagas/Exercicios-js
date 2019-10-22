53a439ce7096000c1d6736faa035122f0fc17fe4
Marcelino F. G. das Chagas <marcelinoprepara@gmail.com>
2019-10-22 12:46:19 -0300

    Fundamentos  JavaScript
    Fundamentos sobre a linguagem JavaScript.

---
+++ fundamentos/ternario.js
@@ -0,0 +1,6 @@
+// Atribui Funcao a resultado
+// Funcao so tem 1 unico parametro
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+console.log(resultado(7.1))
+console.log(resultado(6.7))
